007c6183ab664da2875162885ae7734ac91a7e51
Daniel Sandberg <dsandber@andrew.cmu.edu>
2016-12-29 10:42:21 -0500

    Got tests running

--- test/RubiksCore.Test.Net45/Properties/AssemblyInfo.cs
+++ test/RubiksCore.Test.Net45/Properties/AssemblyInfo.cs
@@ -1,37 +1,36 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RubiksCore.Net45")]
+[assembly: AssemblyTitle("RubiksCore.Test.Net45")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RubiksCore.Net45")]
+[assembly: AssemblyProduct("RubiksCore.Test.Net45")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18297e2a-c9af-4462-977e-6c130af78f8d")]
+[assembly: Guid("49ced10d-b608-490f-9fb7-58462bb4d2f0")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("RubiksCore.Test.Net45")]
